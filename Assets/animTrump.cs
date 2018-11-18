fc28e1c823bfdbb91fba5d3af80eb3aface45f3f
Fireez <benjamin.gallean@etu.umontpellier.fr>
2018-11-18 09:13:32 +0100

    Initial commit

---
+++ Assets/animTrump.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animTrump : MonoBehaviour {
+
+    private Animation anim;
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+    }
+    void Update()
+    {
+        if (!anim.IsPlaying("walk"))
+            anim.Play("walk");
+    }
+}
